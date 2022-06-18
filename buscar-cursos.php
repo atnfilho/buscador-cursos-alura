bb0ad7cf82a8a8a82c5b24b626201472d1222ab1
atnfilho <atnfilho@yahoo.com.br>
2022-06-18 16:01:36 -0300

    Configuração do psr-4.

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,13 +1,12 @@
 <?php
 
 require 'vendor\autoload.php';
-require 'src\Buscador.php';
 
 use Alura\BuscadorDeCursos\Buscador;
 use \GuzzleHttp\Client;
 use \Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client(['base_uri' => 'https://wwww.alura.com.br/']);
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
 $crawler = new Crawler();
 
 $buscador = new Buscador($client, $crawler);
--- src/Buscador.php
+++ src/Buscador.php
@@ -20,7 +20,6 @@ class Buscador {
         $resposta = $this->httpClient->request('GET', $url);
         $html = $resposta->getBody();
         $this->crawler->addHtmlContent($html);
-
         $elementosCursos = $this->crawler->filter('span.card-curso__nome');
 
         $cursos = [];
